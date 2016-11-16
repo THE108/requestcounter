367fb40952d6d1f45ae150bd5395dd5b0899fe54
achernov <andrey.chernov@lazada.com>
2016-11-16 19:28:30 +0300

    Add timestamp into file meta info and clear outdated request counts

--- models/requestcount/persistent.go
+++ models/requestcount/persistent.go
@@ -50,31 +50,35 @@ func (prc *PersistentRequestCounter) Run() error {
 	}
 
 	// counts[0] - current index
-	length := prc.intervalCount + 1
+	// counts[1] - current index init timestamp
+	length := prc.intervalCount + 2
 
-	// prealloc intervalCount + 1 uint64 values
+	// prealloc intervalCount + 2 uint64 values
 	offset := int64(unsafe.Sizeof(prc.prevCountsSum)) * int64(length)
 	if _, err = prc.file.Seek(offset, 0); err != nil {
 		prc.closeFile()
 		return fmt.Errorf("error seek file: %s", err.Error())
 	}
 
+	// write byte to change size of the file
 	if _, err = prc.file.Write([]byte{0xFF}); err != nil {
 		prc.closeFile()
 		return fmt.Errorf("error write file: %s", err.Error())
 	}
 
+	// map file in memory
 	prc.mmaped, err = mmap.Map(prc.file, mmap.RDWR, 0)
 	if err != nil {
 		prc.closeFile()
 		return fmt.Errorf("error map file: %s", err.Error())
 	}
 
+	// cast mapped []byte to []uint64
 	header := (*reflect.SliceHeader)(unsafe.Pointer(&prc.mmaped))
 	header.Len, header.Cap = length, length
-
 	prc.counts = *(*[]uint64)(unsafe.Pointer(header))
-	prc.counts[1] = 0
+
+	prc.clearOutdated()
 	prc.calculatePrevCountSum()
 
 	prc.wg.Add(2)
@@ -129,7 +133,7 @@ func (prc *PersistentRequestCounter) Get(ctx context.Context) *RequestCount {
 		prc.mu.Unlock()
 		return nil
 	}
-	index := int(prc.counts[0]) + 1
+	index := int(prc.counts[0]) + 2
 	prc.counts[index]++
 	count = prc.counts[index] + prc.prevCountsSum
 	prc.mu.Unlock()
@@ -141,9 +145,28 @@ func (prc *PersistentRequestCounter) Get(ctx context.Context) *RequestCount {
 	}
 }
 
+func (prc *PersistentRequestCounter) clearOutdated() {
+	prevTimestamp := time.Unix(0, int64(prc.counts[1]))
+	now := time.Now()
+	startIndex := int(prc.counts[0]) + 1
+	for i := 0; i < prc.intervalCount; i++ {
+		t := prevTimestamp.Add(prc.intervalDuration * time.Duration(i))
+		if t.After(now) {
+			continue
+		}
+
+		index := startIndex + i
+		if index >= prc.intervalCount {
+			index = 0
+		}
+
+		prc.counts[index+2] = 0
+	}
+}
+
 func (prc *PersistentRequestCounter) calculatePrevCountSum() {
 	prc.prevCountsSum = 0
-	for _, cnt := range prc.counts[1:] {
+	for _, cnt := range prc.counts[2:] {
 		prc.prevCountsSum += cnt
 	}
 }
@@ -157,7 +180,12 @@ func (prc *PersistentRequestCounter) shift() {
 		prc.counts[0] = 0
 	}
 
-	prc.counts[int(prc.counts[0])+1] = 0
+	// set timestamp milliseconds
+	prc.counts[1] = uint64(time.Now().UnixNano())
+
+	// set current request count to 0
+	prc.counts[int(prc.counts[0])+2] = 0
+
 	prc.calculatePrevCountSum()
 
 	prc.mu.Unlock()
